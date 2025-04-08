3529762da377e308d55be69876ced61879280068
soap2705 <sam.k.brooks@gmail.com>
2025-04-08 15:50:15 -0400

    updated minor positions

--- Assets/VideoController.cs
+++ Assets/VideoController.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 public class VideoController : MonoBehaviour
 {
+    private VideoPlayer videoPlayer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+ 
+      videoPlayer = GetComponent<VideoPlayer>();
+      if (videoPlayer != null)
+        {
+          videoPlayer.Play();
+            }
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+
